4ea49db9f88f75219eb85303b4ab306581a57318
Bruno Costa <bruno.costa@hardquarters.com>
2024-05-02 17:42:42 +0100

    list: lint metadata controller

--- src/routes/metadata.ts
+++ src/routes/metadata.ts
@@ -6,31 +6,29 @@ import { execute, createMetadata } from '@mintbase-js/sdk';
 export async function postMetadata(req: Request, res: Response) {
 
   const handleCreateMetadata = async (metadata: Product): Promise<string> => {
-
-    const account = await config.getActorAccount();
-
+    const account  = await config.getActorAccount();
     const response = await execute(
         {account},
         createMetadata({
-            contractAddress: process.env["CONTRACT_ADDRESS"],
-            metadata: {
-                    title: metadata.title,
-                    description: metadata.description,
-                    media: 'https://i.ibb.co/0yQf0Gw/IMG-20220905-162735-1.jpg',
-                },
-            noReference: true,
-            maxSupply: metadata.copies,
-            price: metadata.price,
-            // ftAddress: process.env["USDC_ADDRESS"],
-            // ftDecimals: 6,
+          contractAddress: process.env["CONTRACT_ADDRESS"],
+          metadata: {
+            title: metadata.title,
+            description: metadata.description,
+            media: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Google_Verified_Badge.svg/1200px-Google_Verified_Badge.svg.png'
+          },
+          noReference: true,
+          maxSupply: metadata.copies,
+          price: metadata.price ?? 0,
+          // ftAddress: process.env["USDC_ADDRESS"],
+          // ftDecimals: 6,
         })
-
     );
 
     if (response && "receipts_outcome" in response){
       const logReceipt = response.receipts_outcome[0].outcome.logs[0];
-      const log = JSON.parse(logReceipt.substring(11));
-      const metdataID = log.data.metadata_id;
+      const log        = JSON.parse(logReceipt.substring(11));
+      const metdataID  = log.data.metadata_id;
+
       return metdataID;
 
     } else {
@@ -40,11 +38,13 @@ export async function postMetadata(req: Request, res: Response) {
     }
   };
 
-  const productID = req.query.productID as string;
-  const base = await initAirtable();
-  const metadata = await getProductData(base, productID)
+  const productID  = req.query.productID as string;
+  const base       = await initAirtable();
+  const metadata   = await getProductData(base, productID)
   const metadataID = await handleCreateMetadata(metadata);
+
   updateProductStatus(base, productID, metadataID);
+
   res.status(200).send({
     message: `Metadata created successfully for '${metadata.title}'`,
     metadataID: metadataID
